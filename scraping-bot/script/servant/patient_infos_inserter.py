9d288049873cc57c43bcfc01d30c82c6a569c525
Inadati <drumake.unr@gmail.com>
2020-08-24 16:02:03 +0900

    行政が公表するエクセルデータの形式変更によってcronjobが正常に動作しない問題を解決

--- scraping-bot/script/servant/patient_infos_inserter.py
+++ scraping-bot/script/servant/patient_infos_inserter.py
@@ -24,7 +24,7 @@ def serve(db):
 
     # エクセルファイルを開いてシートを読みこむ
     wb = px.load_workbook(env.DL_FILE_NAME, data_only=True)
-    sheet = wb["yousei"]
+    sheet = wb["yousei "]
 
     date = __convert_to_iso(sheet.cell(row=sheet.max_row, column=1).value)
 
@@ -35,8 +35,8 @@ def serve(db):
         _id = uuid.uuid4()
         prefecture_id = "f3344ece-9e94-488a-9811-59f852a4efe9"
         positive = sheet.cell(row=sheet.max_row, column=4).value - sheet.cell(row=sheet.max_row-1, column=4).value
-        discharge = sheet.cell(row=sheet.max_row, column=9).value - sheet.cell(row=sheet.max_row-1, column=9).value
-        death = sheet.cell(row=sheet.max_row, column=8).value - sheet.cell(row=sheet.max_row - 1, column=8).value
+        discharge = sheet.cell(row=sheet.max_row, column=10).value - sheet.cell(row=sheet.max_row-1, column=10).value
+        death = sheet.cell(row=sheet.max_row, column=9).value - sheet.cell(row=sheet.max_row - 1, column=9).value
         sickbed_total = 509
         created_at = __convert_to_iso(datetime.datetime.now())
 
